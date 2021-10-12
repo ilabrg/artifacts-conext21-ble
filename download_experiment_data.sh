2e5b34ee3f2568066dbe3c2d9b1354c2cdc980f4
Hauke Petersen <hauke.petersen@fu-berlin.de>
2021-10-12 17:42:47 +0200

    download_experiment_data.sh: moved raw data to different Nextcloud

--- download_experiment_data.sh
+++ download_experiment_data.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 BASEDIR=$(dirname "${0}")
-SRCLOGS="https://box.fu-berlin.de/s/engt3MkibQQbpTg/download"
-SRCPLOTS="https://box.fu-berlin.de/s/Qg6fqLcGxcsQR6L/download"
+# SRCLOGS="https://box.fu-berlin.de/s/engt3MkibQQbpTg/download"
+SRCLOGS="https://nextcloud.imp.fu-berlin.de/index.php/s/36SDbcbgGMt6EnZ/download"
+# SRCPLOTS="https://box.fu-berlin.de/s/Qg6fqLcGxcsQR6L/download"
+SRCPLOTS="https://nextcloud.imp.fu-berlin.de/index.php/s/FT2nEcZrrGCHqdY/download"
 TMPDIR=$(mktemp -d -t)
 TMPLOGS="logs.zip"
 TMPPLOTS="plots.zip"
